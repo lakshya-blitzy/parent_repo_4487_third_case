1e824057225618b292e1aae131979f21708234b2
lakshya-blitzy <lakshya@blitzy.com>
2026-07-09 20:27:48 +0530

    Create root.py

---
+++ foo/root.py
@@ -0,0 +1,8 @@
+import random
+
+secret = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+if guess == secret:
+    print("You won! 🎉")
+else:
+    print(f"Wrong! It was {secret}. 😢")
